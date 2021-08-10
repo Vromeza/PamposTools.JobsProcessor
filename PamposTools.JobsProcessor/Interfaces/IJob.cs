12267876c0bfe2692678232877db6e1d50e1e6c1
Charalampos Hadjiantoniou <charhadj@gmail.com>
2021-08-10 17:31:11 +0300

    Added support for .NET45, .NET471, NETCore.

--- PamposTools.JobsProcessor/Interfaces/IJob.cs
+++ PamposTools.JobsProcessor/Interfaces/IJob.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PamposTools.JobsProcessor
+﻿namespace PamposTools.JobsProcessor
 {
     /// <summary>
     /// Abstraction on a job
     /// </summary>
     public interface IJob
     {
-        public string Name { get; }
+        string Name { get; }
         JobPriority Priority { get; }
     }
 }
