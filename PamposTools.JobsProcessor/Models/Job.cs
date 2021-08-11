8559b865fd3276f31e69ab6733789f8e47758de5
Charalampos Hadjiantoniou <charhadj@gmail.com>
2021-08-11 17:00:18 +0300

    Fix on Job

--- PamposTools.JobsProcessor/Models/Job.cs
+++ PamposTools.JobsProcessor/Models/Job.cs
@@ -5,13 +5,12 @@ namespace PamposTools.JobsProcessor
     /// <summary>
     /// An abstraction for a job object.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
     public class Job : IJob
     {
         /// <summary>
         /// Auto-generated Guid of the job
         /// </summary>
-        public Guid Id => Guid.NewGuid();
+        public Guid Id { get; private set; }
         public string Name { get; private set; }
         public JobPriority Priority { get; private set; }
 
@@ -22,6 +21,7 @@ namespace PamposTools.JobsProcessor
         /// <param name="priority">Priority of the job</param>
         public Job(string name, JobPriority priority = JobPriority.Medium)
         {
+            Id = Guid.NewGuid();
             Name = name;
             Priority = priority;
         }
