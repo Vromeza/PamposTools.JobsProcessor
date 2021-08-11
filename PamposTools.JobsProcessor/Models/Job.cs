fd94605c7818274510d96647b1d95b594ca1f077
Charalampos Hadjiantoniou <charhadj@gmail.com>
2021-08-11 10:57:07 +0300

    Comments

--- PamposTools.JobsProcessor/Models/Job.cs
+++ PamposTools.JobsProcessor/Models/Job.cs
@@ -8,10 +8,18 @@ namespace PamposTools.JobsProcessor
     /// <typeparam name="T"></typeparam>
     public class Job : IJob
     {
+        /// <summary>
+        /// Auto-generated Guid of the job
+        /// </summary>
         public Guid Id => Guid.NewGuid();
         public string Name { get; private set; }
         public JobPriority Priority { get; private set; }
 
+        /// <summary>
+        /// Initializes an instance of a <see cref="Job"/> specifying the name and optionally specifying the <see cref="JobPriority"/>
+        /// </summary>
+        /// <param name="name">Name of the job</param>
+        /// <param name="priority">Priority of the job</param>
         public Job(string name, JobPriority priority = JobPriority.Medium)
         {
             Name = name;
